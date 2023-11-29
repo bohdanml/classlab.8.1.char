5884903640f6d220435191a0daaf03767f51a1ac
Bogdan <Bogdan@VITOPC>
2023-11-29 20:11:32 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include"..//lab.8.1.char/lab.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char* str = "n2o4no";
+			Assert::AreEqual(Count(str), 1);
+		}
+	};
+}
